c98b6379276b8073b6ee464d753c2d9c5f1a896b
seLain <selain@nature.ee.ncku.edu.tw>
2018-07-16 14:48:42 +0800

    updates for pypi package

--- setup.py
+++ setup.py
@@ -21,19 +21,28 @@ extras_require = {
 
 setup(
     name='django-eel',
-    version='0.1',
+    version='0.11',
     packages=find_packages(),
+    #include_package_data=True, # comment this line to include `package_data`
     package_data={
         'django_eel': ['static/eel/js/eel.js'],
     },
-    include_package_data=True,
     install_requires=requirements,
     extras_require=extras_require,
     license='MIT License',
     description='A Django App for little HTML GUI applications, with easy Python/JS interoperation.',
-    long_description=open('README.md', encoding='utf-8').readlines()[1],
-    url='https://github.com/seLain/django-eel',
-    author='Victor Hu',
+    long_description=''.join(open('README.md', encoding='utf-8').readlines()),
+    long_description_content_type="text/markdown",
+    url='https://github.com/seLain/Django-Eel',
+    download_url = 'https://github.com/seLain/Django-Eel/tarball/v0.1',
+    keywords = ['gui', 'javascript', 'python', 'html', 'django'],
+    author = 'Victor Hu',
     author_email='selain@nature.ee.ncku.edu.tw',
     zip_safe=False, # not being egg
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python :: 3.6',
+        'Framework :: Django :: 2.0',
+        'License :: OSI Approved :: MIT License'],
 )
