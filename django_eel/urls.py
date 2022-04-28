b149e1b0c80e61e2ae768e7e53a336be82b45296
Jatin Sharma <47854465+jatinsharrma@users.noreply.github.com>
2022-04-28 17:18:13 +0530

    Update urls.py
    "from django.conf.urls import url" is deprecated updated to "from django.urls import re_path"

--- django_eel/urls.py
+++ django_eel/urls.py
@@ -13,9 +13,9 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.urls import re_path
 from . import _eel
 
 urlpatterns = [
-    url(r'^eel.js', _eel),
+    re_path(r'^eel.js', _eel),
 ]
